3c0c6fd7cffbe7c782056a8c095ecc94f2195d13
danielcuri <e.curi@pucp.pe>
2025-08-11 18:43:31 -0500

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://ssomac.jjc.com.pe/api',
-  androidVersion: '1.0.0',
+  androidVersion: '1.0.1',
   iosVersion: '1.0.1',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiUrl: 'https://ssomac.jjc.com.pe/api',
-  androidVersion: '1.0.0',
+  androidVersion: '1.0.1',
   iosVersion: '1.0.1',
 };
 

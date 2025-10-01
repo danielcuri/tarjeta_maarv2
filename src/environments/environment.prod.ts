df3a4fe1afc7c7c0e3afe07ab96c27520f42f654
JonatanCuri07 <u201912404@upc.edu.pe>
2025-09-30 20:11:40 -0500

    fix: change label

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiUrl: 'https://ssomac.jjc.com.pe/api',
   //apiUrl: 'http://localhost:4500',
   androidVersion: '1.0.3',
-  iosVersion: '1.0.3',
+  iosVersion: '1.0.4',
 };

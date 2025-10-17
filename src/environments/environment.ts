6c3a38caf1fe008d287e9ae5b0dbe083b14f82e3
AlfredoFarro <u201911924@upc.edu.pe>
2025-10-17 14:40:57 -0500

    fix:adding_filters

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  //apiUrl: 'https://ssomac.jjc.com.pe/api',
-  apiUrl: 'http://localhost:4500',
+  apiUrl: 'https://ssomac.jjc.com.pe/api',
+  //apiUrl: 'http://localhost:4500',
   androidVersion: '1.0.3',
   iosVersion: '1.0.4',
 };
